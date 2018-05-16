c2b3569520150fa06b8f1ab079a6328dd9daf0a6
Siriwimon <jaa.siriwimon@gmail.com>
2018-05-16 11:18:43 +0700

    query resources in findemptyroom

--- app.js
+++ app.js
@@ -11,6 +11,7 @@
 
 var mongojs = require('mongojs');			
 var db = mongojs('test-RBS',['test-RBS']);
+var rooms = [];
 
 // var moment = require('moment');
 // var dbCollections = ["queueRequest","serviceRecord"];
@@ -33,6 +34,12 @@ function isEmptyObject(obj) {
   }
   return true;
 }
+
+// This should compare to array and return differance
+Array.prototype.diff = function(a) {
+    return this.filter(function(i) {return a.indexOf(i) < 0;});
+
+};
 
 module.exports = function(app) {
 
@@ -208,6 +215,8 @@ module.exports = function(app) {
 	});
 
 	// ============ end of main page  ==============
+
+	// ============ begin of check id page ==============
 	app.get('/checkid', function (req,res){
 		// res.render('home.html');
 		res.sendFile('checkID.html',{root : __dirname + '/app'});
@@ -219,11 +228,190 @@ module.exports = function(app) {
 		res.json("checkid test is ok!!");
 	});
 
+	// ============ end of check id page  ==============
+
+
+	// ============ begin of find empty rooms page ==============
+
 	app.get('/findemptyroom', function (req,res){
-		// res.render('home.html');
+		
 		res.sendFile('findEmptyRoom.html',{root : __dirname + '/app'});
 	});
 
+	app.get('/findemptyroom/refresh/:text', function (req,res){
+
+		var text= req.params.text;
+		var arr = text.split(",");
+
+		// assume room from DB
+		// var rooms = ["ubuntu","cent-OS","fedora","solaris","debian"]
+		
+		var hours = 3;	
+		var ms = 60*60*1000;
+
+		var emptyRoom = [];
+		var getEmptyRoom = [];
+		var getResourceName = [];
+		var getRoomName = [];		
+
+		var i = 0;
+
+		
+		var startTime = new Date(moment.tz(new Date(arr[0]),"Asia/Bangkok"));				
+		var	endTime = new Date(moment.tz(new Date(arr[1]),"Asia/Bangkok"));
+		
+ 		var refStart = new Date(moment.tz(new Date(arr[0]),"Asia/Bangkok"));
+ 	    var endholiday = refStart.setTime(refStart.getDate() + 1);
+ 		
+ 		var chkRoomData = arr[2];
+
+		console.log(text);
+		db.events.aggregate(
+			[
+				{ $match: 
+					{$or:[
+						{$and:[
+								{status: 0},
+								{$or:[
+										{$and:[
+												{start: {$lte: startTime}},
+												{end: {$gte: endTime}}
+											]
+										},
+										{start: {$gte: startTime,$lt: endTime}},
+										{end: {$gt: startTime,$lte: endTime}}
+									]
+								}
+							]
+						},
+						{$and:[
+								{status:2},		            		
+				            	{start: {$gte: startTime,$lte: endTime}},
+							]
+						}
+					]
+				} 
+				},{
+			        $lookup:
+			        {
+			            from: "resources",
+			            localField: "resourceId",
+			            foreignField: "children.id",
+			            as: "resourceInfo"
+			        }
+			    },{
+			    	$unwind: {
+			    		path:'$resourceInfo',
+			    		preserveNullAndEmptyArrays: true
+			    	}
+			    }
+			    ,{
+			        $project: {	
+			        	_id: 0,		        	
+			            resourceName: {
+			            	$map: {
+			            		input:{
+			            			$filter: {
+					            		input: "$resourceInfo.children",
+					            		as: "child",
+					            		cond:{	            			
+					            			$eq:["$$child.id","$resourceId"]
+					            			
+					            		}
+					            	}
+
+			            		},
+			            		as: "room",
+			            		in: "$$room.title"
+			            	}
+					            	
+			            }          
+			        }
+		    	},{
+			    	$unwind: {
+			    		path:'$resourceName',
+			    		preserveNullAndEmptyArrays: true
+			    	}
+			    }
+		    ]
+		).toArray(function (err, docs){
+			// console.log(docs.length)
+			getRoomName.splice(0,getRoomName.length);	
+			emptyRoom.splice(0,emptyRoom.length);
+
+			docs.forEach(function(doc){
+				getRoomName.push(doc.resourceName);
+			});		
+			
+
+			if(chkRoomData == 0){
+				rooms.splice(0,rooms.length);
+				db.resources.aggregate( 
+					[
+						{
+					    	$unwind: {
+					    		path:'$children',
+					    		preserveNullAndEmptyArrays: true
+					    	}
+					    }
+					    ,{
+					        $project: {	        	
+					          
+					            _id: 0,         
+					            id: '$children.id',
+					            title: '$children.title',
+					            eventColor: '$children.eventColor'
+
+					        }
+				    	}			    	
+					]
+				).toArray(function (err, resource_docs){
+					rooms.splice(0,rooms.length);
+					getResourceName.splice(0,getResourceName.length);
+					rooms = resource_docs;
+					
+					resource_docs.forEach(function(resource){
+						if(resource.id.includes('t')){
+							getResourceName.push(resource.title)
+						}					
+					});
+
+					getEmptyRoom = getResourceName.diff(getRoomName);
+
+					for (var i = 0; i < rooms.length; i++) {
+						getEmptyRoom.forEach(function(room){
+							if (room == rooms[i].title){
+								emptyRoom.push(rooms[i]);
+							}
+						})
+					}
+
+					console.log(emptyRoom);
+					res.json(emptyRoom);
+				})
+			}else{
+				console.log('not refresh')
+				console.log('rooms: ',rooms);
+				console.log('getEmptyRoom: ',getEmptyRoom);
+				getEmptyRoom = getResourceName.diff(getRoomName);
+
+					for (var i = 0; i < rooms.length; i++) {
+						getEmptyRoom.forEach(function(room){
+							if (room == rooms[i].title){
+								emptyRoom.push(rooms[i]);
+							}
+						})
+					}
+
+					console.log(emptyRoom);
+					res.json(emptyRoom);
+			}
+
+		});
+	});	
+	// ============ end of find empty rooms page  ==============
+
+	// ============ begin of student authentication page ==============
 	app.get('/studentauth', function (req,res){
 		// res.render('home.html');
 		res.sendFile('studentAuth.html',{root : __dirname + '/app'});
@@ -233,7 +421,9 @@ module.exports = function(app) {
 		// res.render('home.html');
 		res.redirect('/booking')
 	});
+	// ============ end of student authentication  ==============
 
+	// ============ begin of booking page ==============
 	app.get('/booking', function (req,res){
 		// res.render('home.html');
 		// res.redirect('/studentauth')
@@ -257,6 +447,9 @@ module.exports = function(app) {
 		console.log("back to main")
 		res.redirect('/main')
 	});
+
+	// ============ end of booking page  ==============
+
 
 	app.get('/rulestutor', function (req,res){
 		// res.render('home.html');
--- app/controllers/findEmptyRoom.js
+++ app/controllers/findEmptyRoom.js
@@ -1,5 +1,192 @@
-app.controller("findRoomCtrl",["$scope", "$http", function($scope,$http) {
+app.controller("findRoomCtrl",['$scope','$http','$compile','$filter','$timeout','$uibModal','$log',function($scope,$http,$compile,$filter,$timeout) {
 	// body...
+
+	// ==== initial date & time setting ===
+    
+	var initTime = new Date();
+		initTime.setMinutes(0);
+		initTime.setSeconds(0);
+	$scope.startTime = initTime;
+	$scope.endTime = initTime;	
+
+	var minTime = new Date();
+		minTime.setHours(8);
+		minTime.setMinutes(0);
+	$scope.min = minTime;
+
+	var maxTime = new Date();
+		maxTime.setHours(18);
+		maxTime.setMinutes(0);
+	$scope.max = maxTime;
+
+	var initDate = new Date();
+	// $scope.date = $filter('date')(initDate, "dd.MM.yyyy"); 
+	$scope.date = "";
+	var state = 0;
+
+	// $scope.emptyRooms = [];
+
+	var maxDate = new Date();
+	maxDate.setDate((new Date()).getDate() + 13);
+	
+		
+	//===============================
+
+	// === control date & time selecting ===
+
+	$scope.open = function(){
+    	$scope.popup.opened = true;
+    };
+
+    $scope.popup = {
+    	opened: false
+  	};
+
+  	$scope.dateOptions = {
+    	dateDisabled: disabled,
+    	formatYear: 'yy',
+    	maxDate: maxDate,
+    	minDate: new Date(),
+    	startingDay: 1
+  	};
+
+  	function disabled(data) {
+    	var date = data.date,
+      	mode = data.mode;
+    	return mode === 'day' && (date.getDay() === 0 );
+  	}
+
+	// $scope.open = function($event) {
+	// 	$event.preventDefault();
+	// 	$event.stopPropagation();
+
+	// 	$scope.opened = true;
+		
+	// };  
+
+	$scope.dateChange = function(){
+		state = 1;
+		checkDate = $scope.date;
+		console.log(checkDate);
+	}
+
+	$scope.ctrlStartTime = function(){
+		
+		
+		if ($scope.endTime != ""){	        
+			if (($scope.endTime.getHours() - $scope.startTime.getHours()) >= 3){
+				limitEndTime = new Date($scope.startTime);
+				limitEndTime.setHours(limitEndTime.getHours() + 3);
+				$scope.endTime = limitEndTime;
+				
+				if ($scope.endTime.getTime() > $scope.max.getTime()){
+					$scope.endTime = $scope.max;
+									
+				}
+				
+			}				
+			
+			if ($scope.startTime.getTime() == $scope.endTime.getTime()){
+				limitEndTime = new Date($scope.endTime);
+				limitEndTime.setHours(limitEndTime.getHours() + 1);
+				$scope.endTime = limitEndTime;
+				
+
+				if ($scope.endTime.getTime() > $scope.max.getTime()){
+					$scope.endTime = $scope.max;
+					limitStartTime = new Date($scope.endTime);
+					limitStartTime.setHours(limitStartTime.getHours() - 1);
+					$scope.startTime = limitStartTime;	
+							
+				}
+
+				// if ($scope.)
+			}			
+
+		} else {
+			if ($scope.startTime.getHours() <= 17){				
+				$scope.startTime = initTime;
+				refStartTime = $scope.startTime;
+				endTime = new Date(refStartTime);
+				endTime.setHours(endTime.getHours() + 1);			
+				$scope.endTime = endTime;
+				
+
+			} else {		
+				setStartTime = $scope.max;		
+				$scope.startTime = setStartTime.setHours(setStartTime.getHours() - 1);
+				$scope.endTime = $scope.max;
+				
+			}
+			
+		}
+
+		
+	};
+
+
+
+	$scope.ctrlEndTime = function(){
+		// $scope.endTime = initTime;
+		if ($scope.startTime != ""){		
+			
+			if (($scope.endTime.getHours() - $scope.startTime.getHours()) >= 3){
+				limitStartTime = new Date($scope.endTime);
+				limitStartTime.setHours(limitStartTime.getHours() - 3);
+				$scope.startTime = limitStartTime;
+				
+				if ($scope.startTime.getTime() < $scope.min.getTime()){
+					$scope.startTime = $scope.min;
+					
+				}
+				
+			}
+			
+
+			if ($scope.startTime.getTime() == $scope.endTime.getTime()){
+				limitStartTime = new Date($scope.startTime);
+				limitStartTime.setHours(limitStartTime.getHours() - 1);
+				$scope.startTime = limitStartTime;
+				
+
+				if ($scope.startTime.getTime() < $scope.min.getTime()){
+					$scope.startTime = $scope.min;	
+					limitEndTime = new Date($scope.startTime);
+					limitEndTime.setHours(limitEndTime.getHours() + 1 );
+					$scope.endTime = limitEndTime;
+					
+				}
+			}
+
+			
+
+		} else {
+			if ($scope.endTime.getHours() >= 9){
+				refEndTime = $scope.endTime;
+				startTime = new Date(refEndTime);
+				startTime.setHours(startTime.getHours() - 1);
+				$scope.startTime = startTime;
+				
+			} else {
+				setEndTime = $scope.min;	
+				$$scope.endTime = setEndTime.setHours(setEndTime.getHours() + 1);
+				$scope.start = $scope.min;
+				
+			}
+		}		
+		
+		
+	};	
+
+	$scope.ctrlStartTime();
+	$scope.ctrlEndTime();
+
+	$scope.hstep = 1;
+  	$scope.mstep = 15;
+  	$scope.ismeridian = true;
+
+	// =====================================
+
 	$scope.project_title = "ระบบจองห้อง";
 	$scope.main = function(){
 		$http.get('/findemptyroom').then(successCallback,errorCallback);
@@ -13,34 +200,45 @@ app.controller("findRoomCtrl",["$scope", "$http", function($scope,$http) {
 			console.log(error);
 		};
 	}
+    
+    roomData = 0;
+    
+	$scope.find = function(){
+		
+		var selectedDate;
 
-	$scope.mainroute = function(){
-		$http.get('/findemptyroom/test').then(successCallback,errorCallback);
+		if ($scope.date != ""){
+			date = new Date($scope.date)
+			selectedDate = $filter('date')(date, "yyyy-MM-dd"); 
+		}else{
+			selectedDate = $filter('date')(initDate, "yyyy-MM-dd");
+		}
+
+		console.log($scope.date);
+
+		var selectedEndTime = $filter('date')($scope.endTime, "HH:mm:ss");
+			selectedEnd = selectedDate + 'T' + selectedEndTime;
+
+		var selectedStartTime = $filter('date')($scope.startTime, "HH:mm:ss");
+			selectedStart = selectedDate + 'T' + selectedStartTime;
+		
+		var timeRange = [selectedStart,selectedEnd,roomData];
+		
+		console.log(timeRange)
+		$http.get('/findemptyroom/refresh/' + timeRange).then(successCallback,errorCallback);
 
 		function successCallback(response){
 			console.log("OK! main");
-			$scope.test = response.data;
-			
+			$scope.emptyRooms = response.data;
+			console.log($scope.emptyRooms);
+			roomData = 1;
 		};		
 
 		function errorCallback(error){
 			console.log(error);
 		};
 	}
-
-	$scope.date = new Date();
-  
-	$scope.open = function($event) {
-		$event.preventDefault();
-		$event.stopPropagation();
-
-		$scope.opened = true;
-	};  
-
-	$scope.hstep = 1;
-  	$scope.mstep = 15;
-  	$scope.ismeridian = true;
-  	$scope.changed = function () {
-	    $log.log('Time changed to: ' + $scope.mytime);
-	};
+	
+    $scope.find();
+  	
 }]);
